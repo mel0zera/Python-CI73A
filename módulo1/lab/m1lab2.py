cef2a8286bf7d2daa082d8b8e27b02e1540e3dfa
mel0zera <48527262+mel0zera@users.noreply.github.com>
2019-03-13 21:08:03 -0300

    Update m1lab2.py

--- módulo1/lab/m1lab2.py
+++ módulo1/lab/m1lab2.py
@@ -1,5 +1,5 @@
 # importa a lib para obter as tabelas da Wikipedia
-from lib.scrapy_table import Scrapy_Table
+from  lib.scrapy_table import Scrapy_Table
 
 # Variavel com o link da tabela
 url="https://pt.wikipedia.org/wiki/C%C3%A2mara_Municipal_de_S%C3%A3o_Paulo"
@@ -10,11 +10,17 @@ site_connect = Scrapy_Table(url)
 # Pegando a tabela 5 (Vereadores em exercicio)
 tables = site_connect.get_tables(5)
   
-# Listando o conteudo da tabela
+# Cria um set porque nao queremos partidos repetidos
+partidos = set() 
+
+# Listando a tabela
 for linha in tables[1:]:
 
-     # Obtendo o nome. Esta na primeira posicao da lista
-     nome = linha[0]
+    # Obtendo o nome. Esta na primeira posicao da lista
+    partidos.add(linha[1])
 
-     # Imprimindo os caracteres da posicao 0 ate 8 
-     print(nome[0:9].replace(' ','.'))
+partidos=sorted(partidos)
+
+# Imprimindo os partidos
+for partido in partidos:
+    print(partido)
