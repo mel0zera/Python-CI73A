191276ab4302911caebd8524b365fbd5b1bb85b1
mel0zera <48527262+mel0zera@users.noreply.github.com>
2019-03-20 22:04:22 -0300

    Create 07.py

---
+++ módulo1/exercicios/07.py
@@ -0,0 +1,28 @@
+from  lib.scrapy_table import Scrapy_Table
+
+
+url_populacao   = "https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_popula%C3%A7%C3%A3o"
+url_homicidio   = "https://pt.wikipedia.org/wiki/Lista_de_cidades_por_taxa_de_homic%C3%ADdios"
+url_fecundidade = "https://pt.wikipedia.org/wiki/Lista_de_unidades_federativas_do_Brasil_por_taxa_de_fecundidade"
+
+
+wiki_populacao   = Scrapy_Table(url_populacao)
+wiki_homicidio   = Scrapy_Table(url_homicidio)
+wiki_fecundidade = Scrapy_Table(url_fecundidade)
+
+
+tables_populacao   = wiki_populacao.get_tables(0)
+tables_homicidio   = wiki_homicidio.get_tables(1)
+tables_fecundidade = wiki_fecundidade.get_tables(1)
+
+tot = 0
+
+for linha in tables_fecundidade[1:]:
+  
+  fec = linha[2]
+  fec = fec.replace(',','.')
+  fecint = float(fec)
+  if (fecint < 2.10):
+    tot = tot + 1
+
+print(tot)
