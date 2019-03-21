c2ce589ada00700277392913ae6af99f729d60db
mel0zera <48527262+mel0zera@users.noreply.github.com>
2019-03-20 21:42:55 -0300

    Create 02.py

---
+++ módulo1/exercicios/02.py
@@ -0,0 +1,26 @@
+from  lib.scrapy_table import Scrapy_Table
+
+
+url_populacao   = "https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_popula%C3%A7%C3%A3o"
+url_homicidio   = "https://pt.wikipedia.org/wiki/Lista_de_cidades_por_taxa_de_homic%C3%ADdios"
+url_fecundidade = "https://pt.wikipedia.org/wiki/Lista_de_unidades_federativas_do_Brasil_por_taxa_de_fecundidade"
+
+
+wiki_populacao   = Scrapy_Table(url_populacao)
+wiki_homicidio   = Scrapy_Table(url_homicidio)
+wiki_fecundidade = Scrapy_Table(url_fecundidade)
+
+
+tables_populacao   = wiki_populacao.get_tables(0)
+tables_homicidio   = wiki_homicidio.get_tables(1)
+tables_fecundidade = wiki_fecundidade.get_tables(1)
+
+
+for linha in tables_populacao[1:]:
+  
+  pop = linha[4]
+  nome = linha[2]
+  pop1 = int(pop)
+
+  if (pop1 < 70000):
+    print(nome)
